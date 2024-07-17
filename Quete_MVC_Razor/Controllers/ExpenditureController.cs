2461d556e0bef562a8e121290790482883084d51
ABT Aurelien <aurelien@abrassart.net>
2024-07-17 13:58:18 +0200

    Ajoutez des fichiers projet.

---
+++ Quete_MVC_Razor/Controllers/ExpenditureController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Quete_MVC_Razor.Models;
+
+namespace Quete_MVC_Razor.Controllers
+{
+    public class ExpenditureController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult GetAll()
+        {
+            List<Expenditure> expenditureList = new List<Expenditure>()
+            {
+                new Expenditure()
+                {
+                    Id = 1,
+                    Name = "Article 1",
+                    Date = DateTime.Now,
+                    Price = 1
+                },
+                   new Expenditure()
+                {
+                    Id = 2,
+                    Name = "Article 2",
+                    Date = DateTime.Now,
+                    Price = 2
+                },
+                      new Expenditure()
+                {
+                    Id = 3,
+                    Name = "Article 3",
+                    Date = DateTime.Now,
+                    Price = 3
+                }
+            };
+
+            return View(expenditureList);
+        }
+    }
+}
---
+++ Quete_MVC_Razor/Models/Expenditure.cs
@@ -0,0 +1,10 @@
+﻿namespace Quete_MVC_Razor.Models
+{
+    public class Expenditure
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public DateTime Date { get; set; }
+        public double Price { get; set; }
+    }
+}
